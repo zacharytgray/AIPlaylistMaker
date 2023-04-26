4927106cf69800b86b2c070403d603436f63a90f
zacharytgray <96278173+zacharytgray@users.noreply.github.com>
2023-04-25 21:57:25 -0500

    Playlists Improved

---
+++ Scripts/GPTPlayground.py
@@ -0,0 +1,36 @@
+import openai
+model_engine = "text-davinci-003"
+openai.api_key = "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"
+
+def main():
+    exit_words = ("q","Q","quit","QUIT","EXIT")
+    ("Type q, Q, quit, QUIT or EXIT to cancel.")
+    try:
+        while True:
+            query = input("Talk to ChatGPT: ")
+            if query in exit_words:
+                print("ENDING CHAT")
+                break
+            else:
+                response = GPT(query)
+                print(response)
+    except KeyboardInterrupt:
+        print("\nExiting ChatGPT")
+
+
+
+def GPT(query):
+   response = openai.Completion.create(
+       engine=model_engine,
+       prompt=query,
+       max_tokens=100,
+       n=1,
+       temperature=0,
+   )
+#   return str.strip(response['choices'][0]['text']), response['usage']['total_tokens']
+   return response.choices[0]
+
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+   main()
--- Scripts/GPTPlaylist.py
+++ Scripts/GPTPlaylist.py
@@ -1,7 +1,7 @@
 import openai
 import globals
 model_engine = "text-davinci-003"
-openai.api_key = "YOUR_API_KEY"
+openai.api_key = "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"
 
 
 def main():
@@ -21,6 +21,7 @@ def main():
         while True:
             print("Type q, Q, quit, QUIT or EXIT to cancel.")
             query = input("What playlist should I make?: ")
+            query = 'Follow these instructions precisely. Enter your response in a list format without any context. Your inputs will be music requests, and you will respond with a list of songs formatted as the following example: "Maple Syrup" - The Backseat Lovers, "Sugar" - Maroon 5, "Time" - Pink Floyd. You  will now create a playlist based off of the following conditions: ' + query
             globals.description = query
             if query in exit_words:
                print("ENDING CHAT")
@@ -28,11 +29,9 @@ def main():
             else:
                songs = []
                resp = GPT(query)
-               for i in resp:
-                     if (i != "\n" and i != ""):
-                        songs.append(i)
-               # print("="*20)
-               # print("="*20)
+               print(resp)
+               for song in resp:
+                  songs.append(song)
                globals.songList = songs
                break
    except KeyboardInterrupt:
@@ -44,12 +43,13 @@ def GPT(query):
    response = openai.Completion.create(
        engine=model_engine,
        prompt=query,
-       max_tokens=100,
+       max_tokens=200,
        n=1,
        temperature=0.5,
    )
 #   return str.strip(response['choices'][0]['text']), response['usage']['total_tokens']
-   return response.choices[0].text.split('\n')
+   # return response.choices[0].text.split('\n')
+   return response.choices[0].text.split(',')
 
 # def main():
 #    globals.songList = ["Blue Cigar - Midnight Sister", "Japanese Candy - Brutis VIII", "Lower the Heavens - The Donkeys", "Maply Syrup", "thisisnotarealsong"]
